e106dc7b31215621bc3d2a786433b68256d1cdf8
devanshpandey2003 <devanshpandey8900@gmail.com>
2025-06-30 22:51:39 +0530

    backgroundJobs

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
+export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .mutation( async ({ input}) => {
+      await inngest.send({
+        name: "hello World",
+        data: {
+          email: input.text,
+        },
+      })
+    }),
+
+    createAI: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
