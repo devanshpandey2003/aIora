65e7992186e7838277c648e5070613064a8cc18f
devanshpandey2003 <devanshpandey8900@gmail.com>
2025-07-08 14:33:37 +0530

    project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import { useMutation, useQuery } from "@tanstack/react-query";
 
@@ -10,28 +11,36 @@ import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
 
-  const { data: messages } = useQuery(trpc.message.getMany.queryOptions());
-  const createMessage = useMutation(
-    trpc.message.create.mutationOptions({
-      onSuccess: () => {
-        toast("message created!");
+  //  const { data: messages } = useQuery(trpc.message.getMany.queryOptions());
+  const createProjects = useMutation(
+    trpc.projects.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
+        toast.success("Project created successfully");
       },
     })
   );
 
   return (
-    <>
-      <div className="p-4">
+    <div className="h-screen flex items-cneter justify-center">
+      <div className="flex max-w-7xl mx-auto items-center flex-col gap-y-4 justify-center">
         <Input value={value} onChange={(e) => setValue(e.target.value)} />
-        <Button onClick={() => createMessage.mutate({ value: value })}>
-          Invoke Background Jobs
+        <Button
+          disabled={createProjects.isPending || !value}
+          onClick={() => createProjects.mutate({ value: value })}
+        >
+          Submit
         </Button>
-        {JSON.stringify(messages, null, 2)}
       </div>
-    </>
+    </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { createTRPCRouter } from "../init";
 
+import { projectsRouter } from "@/modules/porjects/server/preocedures";
 import { messagesRouter } from "@/modules/messages/server/preocedures";
 
 export const appRouter = createTRPCRouter({
   message: messagesRouter,
+  projects: projectsRouter,
 });
 
 export type AppRouter = typeof appRouter;
